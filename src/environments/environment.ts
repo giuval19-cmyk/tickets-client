301a5658cf975e4db3961af43965834eef33f17b
giuval <giuval19@gmail.com>
2026-05-27 13:03:38 +0200

    chore: set gateway to localhost

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
 
   api: {
-    iam: 'http://192.168.1.33:8080/iam-service/auth',
-    support: 'http://192.168.1.33:8080/support-service/api/v1',
-    dss: 'http://192.168.1.33:8080/dss-service/api/v1'
+    iam: 'http://localhost:8080/iam-service/auth',
+    support: 'http://localhost:8080/support-service/api/v1',
+    dss: 'http://localhost:8080/dss-service/api/v1'
   }
 };
